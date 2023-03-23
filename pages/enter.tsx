871ad5bafc8b7922717e7d70afad240a2edc665c
leehyeyeong <elto789@naver.com>
2023-03-24 00:29:16 +0900

    focus typo error

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -40,15 +40,16 @@ export default function Enter() {
           </div>
         </div>
         <form className="flex flex-col mt-8">
-          <label className="text-sm font-medium text-gray-700">
+          <label htmlFor="input" className="text-sm font-medium text-gray-700">
             {method === "email" ? "Email address" : null}
             {method === "phone" ? "Phone number" : null}
           </label>
           <div className="mt-1">
             {method === "email" ? (
               <input
+                id="input"
                 type="email"
-                className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 foucs:border-orange-500"
+                className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                 required
               />
             ) : null}
@@ -58,8 +59,9 @@ export default function Enter() {
                   +82
                 </span>
                 <input
+                  id="input"
                   type="number"
-                  className="py-2 px-4 border border-transparent appearance-none w-full border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 foucs:border-orange-500"
+                  className="py-2 px-4 border border-transparent appearance-none w-full border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                   required
                 />
               </div>
