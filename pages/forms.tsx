3edae4999cfb0e6db0d90bbc8949fc1c15c9db5f
LEEHYEYEONG <elto789@naver.com>
2023-04-04 23:52:32 +0900

    enter form

---
+++ pages/forms.tsx
@@ -0,0 +1,18 @@
+import { useForm } from "react-hook-form";
+
+export default function Forms() {
+  const { register } = useForm();
+  return (
+    <form>
+      <input
+        {...register("username")}
+        type="text"
+        placeholder="Username"
+        required
+      />
+      <input type="email" placeholder="Email" required />
+      <input type="password" placeholder="Password" required />
+      <input type="submit" value="Create Account" />
+    </form>
+  );
+}
